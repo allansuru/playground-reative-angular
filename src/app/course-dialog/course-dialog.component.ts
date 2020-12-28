6f5221550e466be9a064f2703ceed9ee15a90092
SuRu <allansuru@hotmail.com>
2020-12-28 11:53:37 -0300

    removing entry components v11 angular

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,14 +1,14 @@
-import {AfterViewInit, Component, Inject} from '@angular/core';
+import { AfterViewInit, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
-import {CoursesService} from '../services/courses.service';
-import {LoadingService} from '../loading/loading.service';
-import {MessagesService} from '../messages/messages.service';
-import {throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
-import {CoursesStore} from '../services/courses.store';
+import { CoursesService } from '../services/courses.service';
+import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { CoursesStore } from '../services/courses.store';
 
 @Component({
     selector: 'course-dialog',
@@ -23,12 +23,12 @@ export class CourseDialogComponent {
 
     form: FormGroup;
 
-    course:Course;
+    course: Course;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
+        @Inject(MAT_DIALOG_DATA) course: Course,
         private coursesStore: CoursesStore,
         private messagesService: MessagesService) {
 
@@ -38,19 +38,20 @@ export class CourseDialogComponent {
             description: [course.description, Validators.required],
             category: [course.category, Validators.required],
             releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
+            longDescription: [course.longDescription, Validators.required]
         });
+
 
     }
 
     save() {
 
-      const changes = this.form.value;
+        const changes = this.form.value;
 
-      this.coursesStore.saveCourse(this.course.id, changes)
-          .subscribe();
+        this.coursesStore.saveCourse(this.course.id, changes)
+            .subscribe();
 
-      this.dialogRef.close(changes);
+        this.dialogRef.close(changes);
 
     }
 
