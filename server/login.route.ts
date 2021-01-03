1fa17193cbe8bacc6065828ba6a4deb9b653c8ad
SuRu <allansuru@hotmail.com>
2021-01-03 13:48:00 -0300

    adicionando rota para criar um novo usuário

--- server/login.route.ts
+++ server/login.route.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
-import { authenticate } from "./db-data";
-
-
+import { authenticate, USERS } from "./db-data";
+import { User } from '../src/app/model/user';
 
 
 export function loginUser(req: Request, res: Response) {
@@ -18,5 +17,36 @@ export function loginUser(req: Request, res: Response) {
   else {
     res.status(403).json('Não existe este usuário');
   }
+
+}
+
+export function createUser(req: Request, res: Response) {
+  const user: User = req.body;
+
+  console.log("Creating new user", JSON.stringify(user));
+
+  const exist = Object.values(USERS).find((us) => us.email === user.email);
+
+  if (exist) {
+
+    res.status(409).json('Email existente!');
+    return;
+  }
+
+  const newUsers = [...Object.values(USERS), user];
+
+  Object.assign(USERS, newUsers);
+
+  console.log(USERS);
+  setTimeout(() => {
+    try {
+
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json('Erro ao salvar um novo usuário');
+    }
+
+
+  }, 2000);
 
 }
--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,18 @@
 
 import * as express from 'express';
-import {Application} from "express";
-import {getAllCourses, getCourseById} from "./get-courses.route";
-import {searchLessons} from "./search-lessons.route";
-import {saveCourse} from './save-course.route';
-import {loginUser} from './login.route';
+import { Application } from "express";
+import { getAllCourses, getCourseById } from "./get-courses.route";
+import { searchLessons } from "./search-lessons.route";
+import { saveCourse } from './save-course.route';
+import { loginUser, createUser } from './login.route';
 
 const bodyParser = require('body-parser');
 
 const app: Application = express();
 
 app.use(bodyParser.json());
+
+app.route('/api/register').post(createUser);
 
 app.route('/api/courses').get(getAllCourses);
 
@@ -21,6 +23,9 @@ app.route('/api/lessons').get(searchLessons);
 app.route('/api/courses/:id').put(saveCourse);
 
 app.route('/api/login').post(loginUser);
+
+app.route('/api/user').post(loginUser);
+
 
 const httpServer = app.listen(9000, () => {
     console.log("HTTP REST API Server running at http://localhost:" + httpServer.address()["port"]);
